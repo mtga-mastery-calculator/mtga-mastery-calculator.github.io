7c439a9fd381e64a7495111ae5c79ca69cf64ac2
Michal Szafranski <szafranski@gmail.com>
2026-04-23 21:14:36 +0200

    fix SOS max level (#4)

--- src/sets.tsx
+++ src/sets.tsx
@@ -126,7 +126,7 @@ export const sets: Set[] = [
     name: "Secrets of Strixhaven",
     startDate: new Date("2026-04-21T15:00:00Z"),
     endDate: new Date("2026-06-23T12:00:00Z"),
-    maxLevel: 70,
+    maxLevel: 60,
   },
   {
     code: "OM2",
